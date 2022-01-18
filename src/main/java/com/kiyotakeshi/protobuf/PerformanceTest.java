806355a4d8646c6279f0086faffffb621433272a
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2022-01-18 22:44:03 +0900

    compare performance json with Protocol buffers

---
+++ src/main/java/com/kiyotakeshi/protobuf/PerformanceTest.java
@@ -0,0 +1,60 @@
+package com.kiyotakeshi.protobuf;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.kiyotakeshi.json.JPerson;
+import com.kiyotakeshi.models.Person;
+
+import java.io.IOException;
+
+public class PerformanceTest {
+
+    public static void main(String[] args) {
+
+        // json
+        JPerson jPerson = new JPerson();
+        jPerson.setName("mike");
+        jPerson.setAge(30);
+        ObjectMapper mapper = new ObjectMapper();
+
+        Runnable json = () -> {
+            try {
+                byte[] bytes = mapper.writeValueAsBytes(jPerson);
+                JPerson person1 = mapper.readValue(bytes, JPerson.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+
+        // protobuf
+        Person mike = Person.newBuilder()
+                .setName("mike")
+                .setAge(30)
+                .build();
+
+        Runnable proto = () -> {
+            try {
+                byte[] bytes = mike.toByteArray();
+                Person mike1 = Person.parseFrom(bytes);
+            } catch (InvalidProtocolBufferException e) {
+                e.printStackTrace();
+            }
+        };
+
+        for (int i = 0; i < 5; i++) {
+            runPerformanceTest(json, "JSON");
+            runPerformanceTest(proto, "PROTO");
+        }
+    }
+
+    private static void runPerformanceTest(Runnable runnable, String method) {
+        long time1 = System.currentTimeMillis();
+        for (int i = 0; i < 5_000_000; i++) {
+            runnable.run();
+        }
+        long time2 = System.currentTimeMillis();
+
+        System.out.println(method + " : " + (time2 - time1) + " ms");
+    }
+}
