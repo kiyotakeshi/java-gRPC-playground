e7a275403f088f54ec98d8b4e3d49d6503912cb3
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2022-01-18 10:06:39 +0900

    byte serialize

--- src/main/java/com/kiyotakeshi/protobuf/PersonDemo.java
+++ src/main/java/com/kiyotakeshi/protobuf/PersonDemo.java
@@ -2,14 +2,33 @@ package com.kiyotakeshi.protobuf;
 
 import com.kiyotakeshi.models.Person;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class PersonDemo {
 
-    public static void main(String[] args) {
-        Person mike = Person.newBuilder()
+    public static void main(String[] args) throws IOException {
+        Person mike1 = Person.newBuilder()
                 .setName("mike")
                 .setAge(29)
                 .build();
 
-        System.out.println(mike.toString());
+        Person mike2 = Person.newBuilder()
+                .setName("mike")
+                .setAge(29)
+                .build();
+
+        System.out.println(mike1);
+
+        // equals method is override
+        System.out.println(mike1.equals(mike2));
+
+        Path path = Paths.get("mike.serialize");
+//        Files.write(path, mike1.toByteArray());
+        byte[] bytes = Files.readAllBytes(path);
+        Person deserializedMike = Person.parseFrom(bytes);
+        System.out.println(deserializedMike);
     }
 }
