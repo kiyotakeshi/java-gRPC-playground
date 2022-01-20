fa439baf36b9c7f988dc42805de0f951295c33e7
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2022-01-21 07:38:35 +0900

    reserved

--- src/main/java/com/kiyotakeshi/protobuf/VersionCompatibilityTest.java
+++ src/main/java/com/kiyotakeshi/protobuf/VersionCompatibilityTest.java
@@ -41,8 +41,21 @@ public class VersionCompatibilityTest {
         // brand: "LG"
         // year: 2018
         // 3: 3
-        Path pathV2 = Paths.get("tv-v2");
-        byte[] bytes = Files.readAllBytes(pathV2);
+        // Path pathV2 = Paths.get("tv-v2");
+        // byte[] bytes = Files.readAllBytes(pathV2);
+
+        // v3 and v1 has same field count(2)
+        // so, mapping `2: 2019`...
+        // brand: "LG"
+        // 2: 2019
+
+        // v4 second field is price
+        // but data is v1 format and mapping year information...
+        // brand: "LG"
+        // price: 2019
+        Path pathV1 = Paths.get("tv-v1");
+        byte[] bytes = Files.readAllBytes(pathV1);
+
         System.out.println(
                 Television.parseFrom(bytes)
         );
